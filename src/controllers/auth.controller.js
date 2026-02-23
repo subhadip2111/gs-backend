e6eb2e78ef4de86b17c8782821f40100a0a96744
subhadip2111 <subhadipshee20010521@gmail.com>
2026-02-23 08:41:50 +0530

    feat:added new apis accessToken

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,7 +22,7 @@ const logout = catchAsync(async (req, res) => {
 
 const refreshTokens = catchAsync(async (req, res) => {
   const tokens = await authService.refreshAuth(req.body.refreshToken);
-  res.send({ ...tokens });
+  res.send({accessToken:tokens.access.token,refreshToken:tokens.refresh.token });
 });
 
 const forgotPassword = catchAsync(async (req, res) => {
